1a393c50824843b6095dc2bae1898c009c577283
Tore Aurstad <tore.aurstad@hemit.no>
2018-12-31 23:21:26 +0100

    Adding final parts of last module of the course

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -27,7 +27,11 @@ var routes = function (Book) {
 
         bookRouter.route('/:bookId')
             .get(function (req, res) {
-                res.json(req.book);
+                var returnBook = req.book.toJSON();
+                returnBook.links = {};
+                var newLink = 'http://' + req.headers.host + '/api/books/?genre=' + returnBook.genre;
+                returnBook.links.FilterByGenre = newLink.replace(' ', '%20%');
+                res.json(returnBook);
             })
             .put(function (req, res) {
                 req.book.title = req.body.title;
